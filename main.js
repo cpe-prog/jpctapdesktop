cda4f2c53d34ea4409f73ec008faf7532e04c8cf
Grian Gajila <griangajila2@gmail.com>
2023-11-21 21:55:03 +0800

    wip

--- main.js
+++ main.js
@@ -1,46 +1,38 @@
-// main.js
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+const fs = require("fs");
 
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
-const path = require("node:path");
+let mainWindow;
 
-const createWindow = () => {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow({
+function createWindow() {
+	mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
 		webPreferences: {
-			preload: path.join(__dirname, "preload.js"),
+			nodeIntegration: true,
 		},
 	});
 
-	// and load the index.html of the app.
-	//   mainWindow.loadFile('index.html')
-	mainWindow.loadURL("http://localhost:3000");
+	mainWindow.loadURL("http://localhost:3000"); // Set the URL of your Next.js app here
 
-	// Open the DevTools.
-	// mainWindow.webContents.openDevTools()
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-	createWindow();
-
-	app.on("activate", () => {
-		// On macOS it's common to re-create a window in the app when the
-		// dock icon is clicked and there are no other windows open.
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	mainWindow.on("closed", function () {
+		mainWindow = null;
 	});
-});
+}
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", function () {
 	if (process.platform !== "darwin") app.quit();
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on("activate", function () {
+	if (mainWindow === null) createWindow();
+});
+
+ipcMain.on("save-file", (event, content) => {
+	// Implement the file saving logic here
+	// For example, saving content to a file named 'output.txt'
+	fs.writeFileSync("output.txt", content);
+});
