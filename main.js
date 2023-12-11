74dd2a9bb5907262f02bbcfabe8d6d3657a40b26
Grian Gajila <griangajila2@gmail.com>
2023-12-11 22:47:02 +0800

    wip

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
-const url = require("url");
-const fs = require("fs");
 
 let mainWindow;
 
@@ -9,20 +6,19 @@ function createWindow() {
 	mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
-		autoHideMenuBar: true,
 		webPreferences: {
 			nodeIntegration: true,
 		},
 	});
 
-	mainWindow.loadFile("index.html"); // Set the URL of your Next.js app here
+	mainWindow.loadFile("index.html");
 
 	mainWindow.on("closed", function () {
 		mainWindow = null;
 	});
 }
 
-app.whenReady().then(createWindow);
+app.on("ready", createWindow);
 
 app.on("window-all-closed", function () {
 	if (process.platform !== "darwin") app.quit();
@@ -32,8 +28,8 @@ app.on("activate", function () {
 	if (mainWindow === null) createWindow();
 });
 
-ipcMain.on("save-file", (event, content) => {
-	// Implement the file saving logic here
-	// For example, saving content to a file named 'output.txt'
-	fs.writeFileSync("output.txt", content);
+// Listen for barcode data from the renderer process
+ipcMain.on("barcode-detected", (event, barcodeData) => {
+	// Send the barcode data back to the renderer to update the UI
+	mainWindow.webContents.send("update-barcode", barcodeData);
 });
